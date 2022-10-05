dfbc1a573fbafbc90f142388ebef1d27dc36f995
mawreh <mawreh1714@gmail.com>
2022-10-05 09:31:44 -0400

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,42 +1,46 @@
-const intro = document.querySelector(".anime");
+const intro = document.querySelector(".intro");
 const video = intro.querySelector("video");
 const text = intro.querySelector("h1");
 //END SECTION
 const section = document.querySelector("section");
 const end = section.querySelector("h1");
 
-const controller = new ScrollMagic.controller();
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
 
-//scences
+//Scenes
 let scene = new ScrollMagic.Scene({
-    duration: 9000,
-    triggerElement: intro,
-    triggerHook: 0
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
 })
-.addIndicators()
-.setPint(intro)
-.addTo(controller);
 
-const textAnim = TweenMax.fromTo(text, 3, {opicity: 1}, {opacity: 0});
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
 
 let scene2 = new ScrollMagic.Scene({
-    duration: 3000,
-    triggerElement: intro,
-    triggerHook: 0
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
 })
-.setTween(textAnim)
-.addTo(controller);
-//video animation
+  .setTween(textAnim)
+  .addTo(controller);
 
-let accelamount = 0.4;
+//Video Animation
+let accelamount = 0.1;
 let scrollpos = 0;
 let delay = 0;
 
 scene.on("update", e => {
-    scrollpos = e.scrollPos / 1000;
+  scrollpos = e.scrollPos / 1000;
 });
 
 setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-}, 33.3);
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
 
+  video.currentTime = delay;
+}, 41.7);
